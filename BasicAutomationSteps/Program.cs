b57667239efd04c6c4771d1bfb77a62d409b6613
saiteja paluru <saiteja26@gmail.com>
2019-03-31 23:40:38 -0500

    Added Rest Services code & Testd in Program.cs

--- BasicAutomationSteps/Program.cs
+++ BasicAutomationSteps/Program.cs
@@ -3,13 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using BasicAutomationSteps.TestCases;
 namespace BasicAutomationSteps
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Sample Testing Project  --");
+            TestRestfulApiServices t = new TestRestfulApiServices();
+            t.GetPlayGround();
+
         }
     }
 }
---
+++ BasicAutomationSteps/TestCases/TestRestfulApiServices.cs
@@ -0,0 +1,29 @@
+﻿using AutomationSelfProject.Utilities;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicAutomationSteps.TestCases
+{
+    class TestRestfulApiServices
+    {
+        RestServicesGetUtility RestServicesGetUtilityInstance = new RestServicesGetUtility();
+        /// <summary>
+        /// Refernce : https://restful-booker.herokuapp.com/apidoc/index.html
+        /// https://restful-booker.herokuapp.com/
+        /// https://www.ultimateqa.com/dummy-automation-websites/
+        /// https://www.techbeamers.com/websites-to-practice-selenium-webdriver-online/
+        /// </summary>
+        public void GetPlayGround()
+        {
+            StringBuilder RestServiceUrl = new StringBuilder(@"https://restful-booker.herokuapp.com/booking");
+            //[{"bookingid":1},{"bookingid":2},{"bookingid":7},{"bookingid":8},{"bookingid":10},{"bookingid":4},{"bookingid":5},{"bookingid":3},{"bookingid":9},{"bookingid":6}]
+            JObject r = RestServicesGetUtilityInstance.GetRequest(RestServiceUrl.ToString());
+
+
+        }
+    }
+}
